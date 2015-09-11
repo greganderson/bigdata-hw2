88a7fe35987ead8b790db1ba14a2324b0466113f
unaveed <unaveed@gmail.com>
2015-09-11 14:16:21 -0600

    Implemented matrix vector multiplication

--- part1.py
+++ part1.py
@@ -1,4 +1,6 @@
 from pyspark import SparkContext, SparkConf
+from numpy import matrix, array
+import re
 
 
 ### CONFIGURATION ###
@@ -10,17 +12,21 @@ conf.set("spark.executor.memory", "4g")
 
 sc = SparkContext(conf=conf)
 
-blocksize = 5
+blocksize = 1 
 
 
 #### CODE STARTS ###
 
 f = sc.textFile('matrices/a_100x200.txt')
+v = sc.textFile('matrices/xx_100.txt')
 
+v_rdd = v.map(lambda x: float(x))
+vv = matrix(v_rdd.collect())
 # Split then convert strings to numbers
-rdd = f.map(lambda s: s.split(' ')).map(lambda row: (int(row[0]) / blocksize, [int(row[0]), int(row[1]), float(row[2])]))
+rdd = f.map(lambda s: s.split(' ')).map(lambda row: (int(row[0]) / blocksize, [float(row[2])])).reduceByKey(lambda a,b: a+b).map(lambda (k,v): v)
 
-result = rdd.collect()
+#result = rdd.collect()
+result = vv * matrix(rdd.collect())
 
 with open('test.txt', 'w') as fl:
 	fl.write(str(result))
