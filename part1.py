31aa656b782858ee564389849631c66cb0390db8
Greg Anderson <greg@infobytes.com>
2015-09-11 14:16:11 -0600

    Now store the matrices and vectors in rdd.

--- part1.py
+++ part1.py
@@ -1,4 +1,5 @@
 from pyspark import SparkContext, SparkConf
+import numpy as np
 
 ### CONFIGURATION ###
 
@@ -14,12 +15,23 @@ blocksize = 5
 
 #### CODE STARTS ###
 
-f = sc.textFile('matrices/a_100x200.txt')
+m = sc.textFile('matrices/a_100x200.txt')
+v = sc.textFile('small.txt')
 
 # Split then convert strings to numbers
-rdd = f.map(lambda s: s.split(' ')).map(lambda row: [int(row[0]), int(row[1]), float(row[2])])
+mat_raw = f.map(lambda s: s.split(' ')).map(lambda row: [int(row[0]), int(row[1]), float(row[2])])
 
-result = rdd.collect()
+vec_raw = v.map(lambda x: float(x))
+
+mat = np.empty([100, 200], dtype=float)
+data = mat.collect()
+for v in data:
+	mat[v[0]][v[1]] = v[2]
+
+
+matrix = sc.parallelize(mat.tolist())
+vector = sc.parallelize(np.matrix(vec_raw.collect()).tolist())
+
 
 with open('test.txt', 'w') as fl:
 	fl.write(str(result))
