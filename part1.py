6a8602b3ccb8c6ef4acb97fe73fac2c9bf35bed0
Greg Anderson <greg@infobytes.com>
2015-09-11 15:04:36 -0600

    Fixed merging errors.

--- part1.py
+++ part1.py
@@ -19,19 +19,24 @@ f = sc.textFile('matrices/a_100x200.txt')
 v = sc.textFile('matrices/xx_200.txt')
 
 # Split then convert strings to numbers
-mat_raw = f.map(lambda s: s.split(' ')).map(lambda row: (int(row[0]) / blocksize, [float(row[2])])).reduceByKey(lambda a,b: a+b).map(lambda (k,v): v)
+mat_raw = f.map(lambda s: s.split(' ')) \
+					 .map(lambda row: (int(row[0]) / blocksize, [float(row[2])])) \
+					 .reduceByKey(lambda a,b: a+b) \
+					 .map(lambda (k,v): v)
+
 vec_raw = v.map(lambda x: [float(x)])
-vv = matrix(vec_raw.collect())
-result = matrix(mat_raw.collect()) * vv
+vec = matrix(vec_raw.collect())
+result = matrix(mat_raw.collect()) * vec
 
 mat = empty([100, 200], dtype=float)
-data = mat.collect()
+data = mat_raw.collect()
 for v in data:
 	mat[v[0]][v[1]] = v[2]
 
-
+# Put the matrices into rdd's
 matrix = sc.parallelize(mat.tolist())
-vector = sc.parallelize(matrix(vec_raw.collect()).tolist())
+vector = sc.parallelize(vec.tolist())
+#vector = sc.parallelize(matrix(vec_raw.collect()).tolist())
 
 
 with open('test.txt', 'w') as fl:
