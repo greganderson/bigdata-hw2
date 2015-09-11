7f77b19d98380c29f9c1a5095865bb563ab5026a
Greg Anderson <greg@infobytes.com>
2015-09-10 22:04:31 -0600

    Testing new data structures.

--- part1.py
+++ part1.py
@@ -1,5 +1,4 @@
 from pyspark import SparkContext, SparkConf
-import json
 
 
 conf = SparkConf()
@@ -16,12 +15,20 @@ def func(x):
 		mat[row[0]] = {}
 	mat[row[0]] = {row[1] : row[2]}
 
+def func(x):
+	row = x.split(' ')
+	if row[0] not in mat:
+		i = len(mat)
+		mat.append([])
+	mat[i].append(row[2])
+
+
 f = sc.textFile('matrices/a_100x200.txt')
 
-mat = {}
+mat = []
 rdd = f.map(func)
 
-result = json.dumps(rdd.collect())
+result = rdd.collect()
 
 with open('test.txt', 'w') as fl:
 	fl.write(result)
