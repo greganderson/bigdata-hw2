2a8c324ff2d959aafa1cce581af1b4ec89f11084
Greg Anderson <greg@infobytes.com>
2015-09-11 11:01:33 -0600

    Now store matrix in a list of lists.
    Note that we are still storing the indices of each item.  Still haven't gotten
    around that yet.

--- part1.py
+++ part1.py
@@ -1,6 +1,8 @@
 from pyspark import SparkContext, SparkConf
 
 
+### CONFIGURATION ###
+
 conf = SparkConf()
 conf.setMaster("local[4]")
 conf.setAppName("reduce")
@@ -9,21 +11,14 @@ conf.set("spark.executor.memory", "4g")
 sc = SparkContext(conf=conf)
 
 
-def func(x):
-	row = x.split(' ')
-	row = [int(row[0]), int(row[1]), float(row[2])]
-	if row[0] not in mat:
-		i = len(mat)
-		mat.append([])
-	mat[i].append(row[2])
-
+### CODE STARTS ###
 
 f = sc.textFile('matrices/a_100x200.txt')
 
-mat = []
-rdd = f.map(func)
+# Split then convert strings to numbers
+rdd = f.map(lambda s: s.split(' ')).map(lambda row: [int(row[0]), int(row[1]), float(row[2])])
 
 result = rdd.collect()
 
 with open('test.txt', 'w') as fl:
-	fl.write(result)
+	fl.write(str(result))
