c975133578a07ce2632fec0a625fd09d718afbf9
Greg Anderson <greg@infobytes.com>
2015-09-12 15:17:39 -0600

    Now correctly compute dot product.

--- part1.py
+++ part1.py
@@ -13,31 +13,34 @@ sc = SparkContext(conf=conf)
 
 #### CODE STARTS ###
 
-f = sc.textFile('matrices/a_100x200.txt')
-g = sc.textFile('matrices/b_200x100.txt')
+f = sc.textFile('1x2.txt')
+g = sc.textFile('2x3.txt')
 
 
 # a = 4x3
 # b = 3x4
 # c = 4x4
 
-a_rows = 100
-b_cols = 100
+a_rows = 1
+b_cols = 3
 
 # Split then convert strings to numbers
 a = f.map(lambda s: s.split(' ')).map(lambda row: (int(row[0]), int(row[1]), float(row[2])))
 b = g.map(lambda s: s.split(' ')).map(lambda row: (int(row[0]), int(row[1]), float(row[2])))
 
 # Map the values to their correct positions in the result matrix
-result_a = a.flatMap(lambda (i, j, value): [((i, k), value) for k in range(b_cols)])
-result_b = b.flatMap(lambda (i, j, value): [((i, k), value) for k in range(a_rows)])
+r1 = a.flatMap(lambda (i, j, value): [((i, k), value) for k in range(b_cols)])
+r2 = b.flatMap(lambda (j, k, value): [((i, k), value) for i in range(a_rows)])
 
-# Do we union these lists then reduce by key?
-a_b = result_a.union(result_b)
 
-answer = a_b.reduceByKey(lambda x,y: x*y)
 
-r = answer.collect()
+z = zip(r1.collect(), r2.collect())
+zc = sc.parallelize(z)
+
+a = zc.map(lambda (x, y): (x[0], x[1]*y[1]))
+answer = a.reduceByKey(lambda x, y: x+y)
+
+r = answer.sortByKey().collect()
 with open('result.txt', 'w') as fl:
 	fl.write(str(r))
 #answer.saveAsTextFile('result')
