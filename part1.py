1da5eff327d86d89af8da8878fd4bb768d2eac73
Jesus <chuy8jay@gmail.com>
2015-09-13 15:42:28 -0600

    I sort the rdds before zipping them, which makes the output work

--- part1.py
+++ part1.py
@@ -1,4 +1,5 @@
 from pyspark import SparkContext, SparkConf
+from numpy import matrix, array, empty
 
 ### CONFIGURATION ###
 
@@ -9,17 +10,64 @@ conf.set("spark.executor.memory", "4g")
 
 sc = SparkContext(conf=conf)
 
-blocksize = 5
-
 
 #### CODE STARTS ###
 
-f = sc.textFile('matrices/a_100x200.txt')
+file1 = 'matrices/2x2a.txt'
+#2x2a.txt
+#0 0 1
+#0 1 2
+#1 0 3
+#1 1 4
+
+file2 = 'matrices/2x2b.txt'
+#2x2b.txt
+#0 0 5
+#0 1 6
+#1 0 7
+#1 1 8
+
+#file1 = 'matrices/1x2.txt'
+#file2 = 'matrices/2x3.txt'
+#file1 = 'matrices/a_100x200.txt'
+#file2 = 'matrices/b_200x100.txt'
+
+f = sc.textFile(file1)
+g = sc.textFile(file2)
+
+#a_rows = 100
+#b_cols = 100
+a_rows = 2
+b_cols = 2
+
 
 # Split then convert strings to numbers
-rdd = f.map(lambda s: s.split(' ')).map(lambda row: (int(row[0]) / blocksize, [int(row[0]), int(row[1]), float(row[2])]))
+a = f.map(lambda s: s.split(' ')).map(lambda row: (int(row[0]), int(row[1]), float(row[2])))
+b = g.map(lambda s: s.split(' ')).map(lambda row: (int(row[0]), int(row[1]), float(row[2])))
 
-result = rdd.collect()
+# Map the values to their correct positions in the result matrix
+r1 = a.flatMap(lambda (i, j, value): [((i, k), value) for k in range(b_cols)])
+r2 = b.flatMap(lambda (j, k, value): [((i, k), value) for i in range(a_rows)])
 
-with open('test.txt', 'w') as fl:
-	fl.write(str(result))
+#Change: Sort them before we actually zip them
+r1 = r1.sortByKey()
+r2 = r2.sortByKey()
+
+#Change: zip r1 & r2 as rdds
+zc = r1.zip(r2)
+'''
+# Pair the elements
+z = zip(r1.collect(), r2.collect())
+#zc = r1.leftOuterJoin(r2)
+zc = sc.parallelize(z)
+'''
+
+# Compute dot product
+a = zc.map(lambda (x, y): (x[0], x[1]*y[1]))
+answer = a.reduceByKey(lambda x, y: x+y)
+
+r = answer.sortByKey().collect()
+
+
+with open('result.txt', 'w') as fl:
+	fl.write(str(r))
